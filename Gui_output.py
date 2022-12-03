c35510af6b907219d3242a9b35b9c00a876fa4dc
tsmiyamoto <taiki_miyamoto@techsword.co.jp>
2022-12-03 12:49:23 +0900

    format: 不要なimportを削除

--- Gui_output.py
+++ Gui_output.py
@@ -1,21 +1,18 @@
-import kanshi_data as kd
-from datetime import datetime as dt
-from datetime import timedelta as td
 import tkinter as tk
-import pdb
+from datetime import datetime as dt
+
 
 class Gui_output:
 
     meishiki = None
     birthday = dt.now()
     sex = -1
-    
+
     def __init__(self, meishiki):
 
         self.meishiki = meishiki
         self.birthday = meishiki.birthday
         self.sex = meishiki.sex
-
 
     def meishiki_output(self):
 
@@ -24,5 +21,3 @@ class Gui_output:
         root.geometry("350x100")
 
         root.mainloop()
-
-        
--- destiny.py
+++ destiny.py
@@ -4,8 +4,8 @@ from Meishiki import Meishiki
 from Unsei import Unsei
 from Analysis import Analysis
 import sys
-            
-if __name__ == '__main__':
+
+if __name__ == "__main__":
 
     # 命式を作る（日干中心）
     meishiki1 = Meishiki(sys.argv, 2)
@@ -26,7 +26,7 @@ if __name__ == '__main__':
     # meishiki2.append_tsuhen()
     # meishiki2.append_twelve_fortune()
     # meishiki2.append_additional_info()
-    
+
     # # 命式を整形して出力する
     # meishiki2.show_meishiki(yojin_num)
     # meishiki2.show_additional_info(False)
@@ -36,23 +36,22 @@ if __name__ == '__main__':
     unsei.append_daiun()
     unsei.append_nenun()
     unsei.append_unsei()
-    
+
     # 鑑定する
     analysis = Analysis(sys.argv)
-    
+
     analysis.scoring_kan(meishiki1)
     analysis.evaluate_kan_strength(meishiki1)
     analysis.show_kan_strength(meishiki1)
-    
+
     # analysis.scoring_kan(meishiki2)
     # analysis.evaluate_kan_strength(meishiki2)
     # analysis.show_kan_strength(meishiki2)
 
     # analysis.evaluate_kan_interval(meishiki1, meishiki2)
     # analysis.evaluate_analysis_type(meishiki1, meishiki2)
-    
+
     # 運勢を整形して出力する
     unsei.show_daiun_nenun()
-    
+
     analysis.show_character(meishiki1)
-    
